f8f67a73354299ef51c021c07c054e04699947de
hex7896 <144613635+hex7896@users.noreply.github.com>
2023-09-20 17:00:24 +0800

    Changed type of members prev and next in base struct
    This is to ensure identical alignment of the members in both the
    base and the derived struct. Ref C99 6.2.5(27).

--- src/dbl_lst_priv.h
+++ src/dbl_lst_priv.h
@@ -1,9 +1,11 @@
 #ifndef DBL_LST_PRIV_H
 # define DBL_LST_PRIV_H
 
-typedef struct dbl_lst_s {
-    void   *prev;
-    void   *next;
-} dbl_lst_t;
+typedef struct dbl_lst_s dbl_lst_t;
+
+struct dbl_lst_s {
+    dbl_lst_t   *prev;
+    dbl_lst_t   *next;
+};
 
 #endif
